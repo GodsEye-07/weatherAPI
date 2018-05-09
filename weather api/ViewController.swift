55d2b137a7c3328856841f5a0009c8dc3235f6b3
Ayush Verma <ayush@go-gaga-app.com>
2018-05-09 19:09:18 +0530

    parsed the api key to get the JSON response

--- weather api/ViewController.swift
+++ weather api/ViewController.swift
@@ -7,18 +7,40 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
+    
+    
+    var url = "https://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=2ca95193405ac2cab082bd4009dbdf9f"
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        guard let finalUrl = URL(string: url) else {
+            return
+        }
+        
+        Alamofire.request(finalUrl, method: .get)
+        
+            .responseJSON { (response) in
+                print(response)
+        }
+
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    
+    
 
 
 }
